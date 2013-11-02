9d609d364a85b106fdd98829c727b6d0eb5dd8e8
michaelminter <mothore@gmail.com>
2013-11-01 20:04:34 -0500

    new rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myinverts::Application.config.secret_key_base = '5065a12b92df2db04f9a537cd1f187c4ca078803a37d43b4e3d6cbfa11d703ebb9336f94b9f80f3408abdbaca7fed58a497d66e3146fcf6a70380686866bb29e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Myinverts::Application.config.session_store :cookie_store, key: '_myinverts_session'
