cffe938c5e17ca8b1cb60edcb3ec36b7d951ae8d
michaelminter <mothore@gmail.com>
2013-11-03 11:27:42 -0600

    add users

--- db/migrate/20131102042637_devise_create_users.rb
+++ db/migrate/20131102042637_devise_create_users.rb
@@ -20,16 +20,31 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string   :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      #t.string   :confirmation_token
+      #t.datetime :confirmed_at
+      #t.datetime :confirmation_sent_at
+      #t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.boolean  :admin, :default => false
+
+      t.string   :first_name
+      t.string   :last_name
+      t.string   :address
+      t.string   :address2
+      t.string   :city
+      t.string   :state
+      t.string   :zip_code
+      t.string   :country
+      t.string   :phone_number
+
+      t.string   :subscription_id
+      t.float    :subscription_price, :default => 0.0 # price at sign up is static
+      t.string   :stripe_id
 
       t.timestamps
     end
